056aa1d79e964b89f780effee15fc489ebe2d5c0
Jean-Francois Numainville <jfnumainville@gmail.com>
2023-06-19 21:36:33 -0400

    Pushing partially completed routing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Auth0 callback route
+  get '/auth/auth0/callback' => 'auth0#callback'
+
+  #user routes
+   resources :users do
+    resources :stories, only: [:index, :show, :update, :destroy] do
+      resources :pages, only: [:show, :create, :update, :destroy]
+    end
+  end
+
+  # Route for creating the first page of a story, which also creates the story itself
+  post '/users/:user_id/stories/first_page', to: 'pages#create_first_page'
 end
