0b797f424d0fdfa7caa2351bac0bca6e99ecef14
Jean-Francois Numainville <jfnumainville@gmail.com>
2023-08-23 20:21:02 -0400

    Added functionality to auto-detect browser language and display text in correct language. Still need to complete translation files

--- frontend/src/__layout.svelte
+++ frontend/src/__layout.svelte
@@ -1,3 +1,21 @@
+<script>
+  import { init, dictionary, register, getLocaleFromNavigator } from 'svelte-i18n';
+
+  // Import translation files
+  import en from './i18n/en.json';
+  import fr from './i18n/fr.json';
+
+    // Initialize the i18n library
+    init({
+      fallbackLocale: 'en',
+      initialLocale: typeof navigator !== 'undefined' ? getLocaleFromNavigator().substring(0,2): "en",
+    });
+
+   //console.log ("The browser language is "+ getLocaleFromNavigator().substring(0,2))
+    // Set the dictionary
+    dictionary.set({ en, fr });
+</script>
+
 <svelte:head>
   <title>Fabulart Layout</title>
 </svelte:head>
