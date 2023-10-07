c10de83381db39826e772e03a16929a586127285
Jean-Francois Numainville <jfnumainville@gmail.com>
2023-10-07 15:26:31 -0400

    Added aliases to the vite.config file, updated homepage import statements to use those aliases

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-    import Layout from '../__layout.svelte';
-    import Navbar from '../lib/Navbar.svelte';
-    import NewStory from '../lib/storyPages/NewStory.svelte';
-    import NewPage from '../lib/storyPages/NewPage.svelte';
-    import HeroBanner from '../lib/banners/HeroBanner.svelte';
-    import SubBanner from '../lib/banners/SubBanner.svelte';
+    import Layout from '$src/__layout.svelte';
+    import Navbar from '$lib/Navbar.svelte';
+    import NewStory from '$lib/storyPages/NewStory.svelte';
+    import NewPage from '$lib/storyPages/NewPage.svelte';
+    import HeroBanner from '$lib/banners/HeroBanner.svelte';
+    import SubBanner from '$lib/banners/SubBanner.svelte';
     import { t } from 'svelte-i18n';
     const user = {
         username: 'Jacob#3254',
@@ -24,7 +24,7 @@ It could be fun for them to pick a "pirate" avatar, which has no effect on the r
     <section>
         <HeroBanner />
     </section>
-    <section id="sub-banner-section" class="sub-banner-section">
+      <section id="sub-banner-section" class="sub-banner-section">
         <div class="section-wave-top">
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                 <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // Define API endpoints for different environments
 const apiEndpoints = {
@@ -18,5 +19,11 @@ export default defineConfig({
     },
     port: 3030,
     host: '0.0.0.0'
+  },
+  resolve:{
+    alias:{
+      $src: path.resolve(__dirname, 'src'),
+      $lib: path.resolve(__dirname, 'src/lib')
+    }
   }
 });
