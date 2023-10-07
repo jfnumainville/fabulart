9d023bd71a2e95e0ad13d2e901525d833a7fea13
Jean-Francois Numainville <jfnumainville@gmail.com>
2023-10-07 16:32:27 -0400

    Removing files from the '.svelte-kit' folder as they do not need to be tracked

--- frontend/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.4dfac617.js","_app/immutable/chunks/index.ab206ff1.js","_app/immutable/chunks/singletons.79086125.js"];
-export const stylesheets = [];
-export const fonts = [];
--- frontend/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.6c76319e.js","_app/immutable/chunks/index.ab206ff1.js"];
-export const stylesheets = ["_app/immutable/assets/2.b9be175d.css"];
-export const fonts = [];
