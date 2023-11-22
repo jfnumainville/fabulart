618bd1f1f71438f093badca8101e988eba8d138f
jfnumainville <jfnumainville@gmail.com>
2023-11-21 22:11:26 -0500

    Created service for calling external API to generate pictures from user prompts

---
+++ backend/app/services/image_generation_service.rb
@@ -0,0 +1,39 @@
+class ImageGenerationService
+
+  def self.generate_image_url (image_prompt)
+     begin
+      connection = Faraday.new('https://api.openai.com/v1/images/generations') do |conn|
+        conn.request :json
+        conn.headers['Authorization'] = "Bearer #{Rails.application.credentials.openai[:api_key]}"
+        conn.adapter Faraday.default_adapter
+      end
+
+        body = {
+        prompt: image_prompt,
+        n: 1,
+        size: '1024x1024',
+        response_format: 'url'
+      }
+      response = connection.post do |req|
+        req.body = body
+      end
+
+      if response.success?
+           return JSON.parse(response.body)['data'][0]['url']
+      else
+        # Log the unsuccessful response details
+        # TODO: Need to have front-end tell user to try again when the image_url value is null.
+        # TODO: Need to setup the log rotation in PROD.
+        Rails.logger.error("DALL-E API Unsuccessful Response: #{response.body}")
+        return nil
+      end
+    rescue Faraday::Error => e
+      # Log the Faraday error details
+      Rails.logger.error("Faraday Error: #{e.message}")
+      return nil
+    end
+  end
+
+
+
+end
